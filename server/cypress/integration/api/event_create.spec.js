d4a13a0a1be2e9a0da5cad6a9f9b5886674a6a46
Anupama Doijad <anupamadoijad@gmail.com>
2022-05-24 08:02:38 +0100

    Added 2 negative scenarios for api endpoints

--- server/cypress/integration/api/event_create.spec.js
+++ server/cypress/integration/api/event_create.spec.js
@@ -1,6 +1,6 @@
 describe('event creation endpoint', () => {
     
-    it.only('creates event - POST', () => {
+    it('creates event - POST', () => {
         cy.request('/events').as('getEvents');
             //create event
             cy.request({
@@ -47,5 +47,28 @@ describe('event creation endpoint', () => {
             }).then((response) => {
                 expect(response.body).to.contain({ error: 'name is to short' })
         })
+    });
+
+    it('sends empty body to the creaate events endpoint', () => {
+        cy.request({
+            method : 'POST', 
+            url : '/events/', 
+            body : {}
+            }).then((response) => {
+                expect(response.body).to.contain({ error: 'name is to short' })            
+            })
+    });
+
+    it('sends unknown key pair values to the creaate events endpoint', () => {
+        cy.request({
+            method : 'POST', 
+            url : '/events/', 
+            body : {
+                "location" : "O2 Academy",
+                "date" : "12-06-2022"
+            }
+            }).then((response) => {
+                expect(response.body).to.contain({ error: 'name is to short' })            
+            })
     });
 })
